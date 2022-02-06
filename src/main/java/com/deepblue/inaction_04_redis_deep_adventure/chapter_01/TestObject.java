3f5c58d60a0ee328e472ce5d5f68faed71b01e6d
yangjianwei <2017585616@qq.com>
2022-02-07 03:42:37 +0800

    Redis深度历险基础应用篇

---
+++ src/main/java/com/deepblue/inaction_04_redis_deep_adventure/chapter_01/TestObject.java
@@ -0,0 +1,14 @@
+package com.deepblue.inaction_04_redis_deep_adventure.chapter_01;
+
+/**
+ *
+ */
+public class TestObject {
+
+    public static void main(String[] args) {
+
+        long value = Long.MAX_VALUE;
+        System.out.println(value);
+
+    }
+}
